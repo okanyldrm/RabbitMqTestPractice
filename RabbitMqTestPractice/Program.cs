8f668598320318702f8e4105e1a2ec47a16afd2e
Okan Yıldırım <okanyldrm68@gmail.com>
2024-12-20 17:22:04 +0300

    Add project files.

---
+++ RabbitMqTestPractice/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMqTestPractice
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+                SendMessage();
+
+            }
+
+
+        }
+
+
+        public static async void SendMessage()
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://qmigkbqr:gOtB2rhWGkjXFWL21mDBFOEpXt2MM4rj@stingray.rmq.cloudamqp.com/qmigkbqr");
+
+
+            using var connection = factory.CreateConnectionAsync();
+
+
+            var channel = connection.Result.CreateChannelAsync();
+
+            channel.Result.QueueDeclareAsync("message-queue", true, false, false);
+
+            var mesaj = "test mesaj";
+
+            var body = Encoding.UTF8.GetBytes(mesaj);
+            //String.Empty, "message-queue",null,body
+            await channel.Result.BasicPublishAsync(String.Empty, "message-queue", body);
+
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
+
+
+
+
---
+++ RabbitMqTestPracticeGetMessage/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMqTestPracticeGetMessage
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://qmigkbqr:gOtB2rhWGkjXFWL21mDBFOEpXt2MM4rj@stingray.rmq.cloudamqp.com/qmigkbqr");
+
+
+            using var connection = factory.CreateConnectionAsync();
+
+
+            var channel = connection.Result.CreateChannelAsync();
+
+            channel.Result.QueueDeclareAsync("message-queue", true, false, false);
+
+
+            var consumer = new AsyncEventingBasicConsumer(channel.Result);
+
+            await channel.Result.BasicConsumeAsync("message-queue", true, consumer);
+
+            consumer.ReceivedAsync += Consumer_ReceivedAsync; ;
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        private static async Task Consumer_ReceivedAsync(object sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine("Request Message : " + Encoding.UTF8.GetString(e.Body.ToArray()));
+        }
+    }
+}
